c77e82d142e99162b0ac114f5428ed60a530ac43
Sofia Chupyra <sofiachupyra@macbook-air-sofia.home>
2021-07-26 21:39:13 +0200

    lesson07

--- t03/lesson06.c
+++ t03/lesson06.c
@@ -2,6 +2,10 @@
 #include <math.h>
 
 int main(void){
-	
+	double a = 0.1;
+	double b = 0.2;
+	double x = 1.0;
+	double r = (exp(5*log(sqrt(pow(x,2)+b)))) - pow(b,2)*pow(sin(x + a),3)/x;
+	printf("result = %.5f\n", r);
 	return 0;
 }
---
+++ t03/lesson07.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+	int x = 0;
+	if (x < 0)
+	{
+		printf("%s\n", "positive");
+	}
+	else printf("%s\n","negative" );	
+
+	return 0;
+}
